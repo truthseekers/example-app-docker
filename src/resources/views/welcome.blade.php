fb51f3b67538b2a442ff08bbf535ad12d21b933c
John Curry <fastpenguin91@gmail.com>
2023-04-25 15:46:55 -0700

    force https on prod middleware

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -57,6 +57,8 @@
                                 <p>password: {{env('DB_PASSWORD', '')}}</p>
                                 <p>socket: {{env('DB_SOCKET', '')}}</p>
                                 <p>url: {{env('DATABASE_URL')}}</p>
+                                <p>APP_ENV: {{env('APP_ENV')}}</p>
+                                <p>APP_ENV: {{env('APP_ENV')}}</p>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                                     Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
